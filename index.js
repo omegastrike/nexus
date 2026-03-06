a143835680c55d8a17f97b6b79cdbc1ad1b5c7be
Octane <115769802+omegastrike@users.noreply.github.com>
2026-03-07 00:15:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
+
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const commandHandler = require("./handlers/commandHandler");
+const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
   intents: [
@@ -11,6 +15,10 @@ const client = new Client({
 });
 
 client.commands = new Collection();
+
+// Load handlers AFTER client is created
+eventHandler(client);
+commandHandler(client);
 
 client.once("ready", () => {
   console.log(`Bot online: ${client.user.tag}`);
