1f2b5d225c58285781345ceb89328c1ce321d297
Octane <115769802+omegastrike@users.noreply.github.com>
2026-03-08 23:40:35 +0530

    Delete events/events directory

--- events/events/messageUpdate.js
+++
@@ -1,9 +0,0 @@
-const logEdit = require("../systems/logs/messageEdit");
-
-module.exports = {
-  name: "messageUpdate",
-
-  execute(oldMessage, newMessage) {
-    logEdit(oldMessage, newMessage);
-  }
-};
