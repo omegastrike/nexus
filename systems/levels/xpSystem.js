780edf807d6ea5a6b3f8ee1f47f990b16c43e10f
Octane <115769802+omegastrike@users.noreply.github.com>
2026-03-12 04:28:46 +1030

    Update xpSystem.js

--- systems/levels/xpSystem.js
+++ systems/levels/xpSystem.js
@@ -1,1 +1,39 @@
-..
+const XP = require("../../database/models/xpModel");
+
+module.exports = async (message) => {
+
+  if (!message.guild || message.author.bot) return;
+
+  let user = await XP.findOne({
+    userId: message.author.id,
+    guildId: message.guild.id
+  });
+
+  if (!user) {
+
+    user = new XP({
+      userId: message.author.id,
+      guildId: message.guild.id
+    });
+
+  }
+
+  const xpGain = Math.floor(Math.random() * 10) + 5;
+
+  user.xp += xpGain;
+
+  const nextLevel = user.level * 100;
+
+  if (user.xp >= nextLevel) {
+
+    user.level++;
+
+    message.channel.send(
+      `🎉 ${message.author} leveled up to **Level ${user.level}**!`
+    );
+
+  }
+
+  await user.save();
+
+};
