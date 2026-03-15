f8bb1f37874f42d39ab33c0c3302a73d1bd5adc3
Octane <115769802+omegastrike@users.noreply.github.com>
2026-03-15 23:43:37 +0530

    Update slashCommandHandler.js

--- handlers/slashCommandHandler.js
+++ handlers/slashCommandHandler.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
+const path = require("path");
 
 module.exports = (client) => {
 
   client.slashCommands = new Map();
 
-  const commandFiles = fs
-    .readdirSync("./slashCommands")
-    .filter(file => file.endsWith(".js"));
+  const commandsPath = path.join(__dirname, "../slashCommands");
+  const commandFiles = fs.readdirSync(commandsPath);
 
   for (const file of commandFiles) {
 
     const command = require(`../slashCommands/${file}`);
+
+    if (!command.data || !command.execute) {
+      console.log(`⚠ Invalid slash command: ${file}`);
+      continue;
+    }
 
     client.slashCommands.set(command.data.name, command);
 
